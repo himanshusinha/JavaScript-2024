ed0b58fde946fc3f4a61c109a191ce520411ecc5
himanshusinha <himanshusinha011@gmail.com>
2024-01-07 12:40:49 +0530

    first commit

---
+++ JavaScript/01-fundamentals/starter/app.js
@@ -0,0 +1,11 @@
+let js = "amazing";
+console.log(10 + 20 - 5);
+// Jonas is value
+console.log("Jonas");
+// firstName is the name of the variable
+// John is the value
+
+let firstName = "John"; // declaration of variable
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
---
+++ assignments/assignment1.js
@@ -0,0 +1,14 @@
+const country = "India";
+const continent = "Asia";
+const population = " 1,461,898,454";
+
+console.log(
+  "Country is " +
+    country +
+    " " +
+    "Continent is " +
+    continent +
+    " " +
+    "Population is " +
+    population
+);
