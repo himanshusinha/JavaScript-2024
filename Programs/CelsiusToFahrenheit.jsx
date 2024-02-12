ac21a439469c676e0c9dd0fbe76e615a4cc6d942
himanshusinha <himanshusinha011@gmail.com>
2024-02-13 00:50:05 +0530

    4 programs done

---
+++ Programs/CelsiusToFahrenheit.jsx
@@ -0,0 +1,3 @@
+const celsius = 25; // Example Celsius value
+const fahrenheit = (celsius * 1.8) + 32;
+console.log(`${celsius} degree Celsius is equal to ${fahrenheit} degree Fahrenheit.`);
---
+++ Programs/EvenOdd.jsx
@@ -0,0 +1,6 @@
+let num=3;
+if(num%2==0){
+  console.log("Number is even")
+}else{
+  console.log("Number is odd")
+}
