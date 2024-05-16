86155fabe33a07368066d6d6be731cf88c2e4962
himanshu sinha <himanshusinha@himanshus-Mini.bbrouter>
2024-05-17 00:29:52 +0530

    17/05/2024

---
+++ JavaScript/01 Fundamentals-Basics/script.js
@@ -0,0 +1,126 @@
+/* console.log("Jonas");
+console.log(23);
+let firstName = "Matlida"; // declaring a variable
+let first_name = "jonas";
+// let 3years = 3;
+//let jonas&matilda = 'JM';
+// let new = 27;
+// let _function = 24; // let $function = 24; // these are allowed in js
+// let Person = 'jonas';
+// let PI = 3.14141;
+// let myFirstJob ='Programmer';
+// let myCurrentJob ='Teacher';
+// let job1 = "programmer";
+// let job2 = "teacher";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName); */
+
+/* let javaScriptIsFun = true;
+console.log(typeof javaScriptIsFun);
+console.log(typeof 23);
+console.log(typeof "jonas");
+javaScriptIsFun = "YES";
+console.log(typeof javaScriptIsFun);
+
+let firstName;
+console.log(typeof firstName);
+
+firstName = "Jonas";
+console.log(typeof firstName);
+console.log(typeof null);
+  */
+
+/* let age = 31;
+age = 32;
+console.log(age); */
+
+/* const birthYear = 1991;
+birthYear = 1990;
+console.log(birthYear);
+ */
+
+// const job;
+// console.log(job);
+/* 
+var age = 20;
+age = 21;
+
+console.log(age); */
+// const now = 2037;
+// const ageJonas = now - 1990;
+// const ageSarah = now - 1991;
+// console.log(ageJonas, ageSarah);
+// console.log(ageJonas * 2, ageSarah / 2, 2 ** 3);
+
+// const firstName = "Jonas";
+// const lastName = "Schmedtmann";
+
+// console.log(firstName + " " + lastName);
+// let x = 10 + 5;
+// x += 5;
+// x *= 4;
+// x++;
+// x--;
+// console.log(x);
+
+// console.log(ageJonas >= ageSarah);
+// const isFullAge = ageSarah >= 18;
+
+// Operators precedences
+
+// const now = 2037;
+// const ageJonas = now - 1990;
+// const ageSarah = now - 1991;
+
+// console.log(now - 1990 > now - 1991);
+
+// let x, y;
+// x = y = 25 - 15 - 5;
+// console.log(x, y);
+
+// const averageAge = ageJonas + ageSarah / 2;
+// console.log(ageJonas, ageSarah, averageAge);
+
+// Template Literals
+// const firstName = "Jonas";
+// const age = 2024 - 1991;
+// const job = "Teacher";
+
+// console.log(
+//   " I'm " +
+//     " " +
+//     firstName +
+//     " " +
+//     " I'm " +
+//     " " +
+//     age +
+//     " year's old " +
+//     " " +
+//     job
+// );
+
+// console.log(`I'm ${firstName} , I'm ${age} year's old , ${job} `);
+// console.log("String's with \n multiple \n lines");
+// console.log(`String with
+// multiple lines`);
+
+// If else
+
+let age = 15;
+if (age >= 18) {
+  console.log("You're eligible for vote ...");
+} else {
+  const yearsLeft = 18 - age;
+  console.log(
+    `You're not eligible for vote  wait another ${yearsLeft} year's...`
+  );
+}
+let century = "";
+const birthYear = 18;
+if (birthYear <= 20) {
+  let century = 20;
+} else {
+  let century = 21;
+}
+console.log(century);
