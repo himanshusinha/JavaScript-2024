a3f8578685615177c3fe72719c8b7717aca5f38d
Macmini <macmini@Macminis-Mini.bbrouter>
2024-12-28 00:34:39 +0530

    latest

---
+++ 02 Fundamentals Part 02/script.js
@@ -0,0 +1,97 @@
+'use strict';
+/*
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense);
+console.log('I can drive :D');
+
+// const interface = 'Audio';
+// const private = 534;
+// const if = 23;
+
+//functions
+// function logger() {
+//   console.log('My name is Jonas');
+// }
+// //calling / running / invoking function
+// logger();
+// logger();
+// logger();
+
+// function fruitProcessor(apples, oranges) {
+//   const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+//   return juice;
+// }
+// const appleJuice = fruitProcessor(5, 0);
+// console.log(appleJuice);
+
+// const orangeJuice = fruitProcessor(2, 4);
+// console.log(orangeJuice);
+
+// const num = Number('23');
+// console.log(num);
+
+// //Funciton declaration and function expressions
+
+// //Function declaration
+// function calcAge1(birthYear) {
+//   return 2037 - birthYear;
+// }
+// const age1 = calcAge1(1991);
+
+// //Function expression
+// const calcAge2 = function (birthYear) {
+//   return 2037 - birthYear;
+// };
+
+// const age2 = calcAge2(1991);
+
+// console.log(age1, age2);
+
+const calcAge3 = birthYear => 2037 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearUntilRetirement = (birthYear, firstName) => {
+  const age = 2037 - birthYear;
+  const retirement = 65 - age;
+  return `${firstName} retires in ${retirement} years`;
+};
+console.log(yearUntilRetirement(1991, 'Jonas'));
+console.log(yearUntilRetirement(1980, 'Bob'));
+
+function cutFruitPieces(fruit) {
+  return fruit * 3;
+}
+
+function foodProcessor(apples, oranges) {
+  const applePieces = cutFruitPieces(apples);
+  const orangePieces = cutFruitPieces(oranges);
+  const juice = `Juice with ${applePieces} apples and ${orangePieces} oranges.`;
+  return juice;
+}
+console.log(foodProcessor(2, 3));
+
+const calcAge1 = function (birthYear) {
+  return 2037 - birthYear;
+};
+const yearsUntilRetirement = (birthYear, firstName) => {
+  const age = calcAge1(birthYear);
+  const retirement = 65 - age;
+  if (retirement > 0) {
+    console.log(`${firstName} retires in ${retirement} years`);
+    return retirement;
+  } else {
+    console.log(`${firstName} retires in ${retirement} years`);
+    return -1;
+  }
+};
+console.log(yearsUntilRetirement(1991, 'Jonas'));
+console.log(yearsUntilRetirement(1970, 'Mike'));
+*/
+
+const friends1 = 'Michael';
+const friends2 = 'Steven';
+const friends3 = 'Peter';
