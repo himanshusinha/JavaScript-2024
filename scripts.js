9d492b6b833cb40848b39b9a1bf87f5767b23242
himanshus-ats <himanshus.ats@gmail.com>
2025-01-25 15:00:03 +0530

    updated

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-let js = "amazing";
+/* let js = "amazing";
 console.log(40 + 8 + 23 - 10);
 
 // Variables name conventions
@@ -18,3 +18,54 @@ let myCurrentJob = "Programmer";
 
 let job1 = "teacher";
 let job2 = "programmer";
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof javascriptIsFun);
+console.log(typeof true);
+console.log(typeof 23);
+console.log(typeof "Jonas");
+
+let year;
+console.log(typeof year);
+year = 1991;
+console.log(typeof year);
+console.log(typeof null);
+
+let age = 30;
+age = 31;
+console.log(age);
+
+// const birthYear = 1991;
+// birthYear = 1990;
+
+// const job;
+// console.log(job) 
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2019;
+console.log(ageJonas, ageSarah, 2 ** 3);
+const firstName = "Jonas";
+const lastName = "Schmedtmann";
+console.log(firstName + " " + lastName);
+
+//Assignment Operators
+let x = 10 + 5; //x=15
+x += 10; //x = 15+10
+x *= 4; //x = 25*4 x = 100
+x--;
+console.log(x);
+
+//Comparison Operators
+console.log(ageJonas > ageSarah); //>,<,>=,<=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+console.log(isFullAge);
+console.log(now - 1991 > now - 2018);
+*/
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2019;
